b232108082f5c833d8ab0eb2446498425cfcb7ab
Pedro Abe <pedrobettiolabe@gmail.com>
2024-12-05 04:23:21 -0300

    updating create node to merge node

--- main/python/connection.py
+++ main/python/connection.py
@@ -10,18 +10,17 @@ class Connection():
         self._driver.close()
 
     @staticmethod
-    def create_node_tx(tx, name, nodeType):
-        create_node = ("""
-            CREATE (n:{_nodeType})
-            SET n.name = "{_name}"
-            RETURN n.id AS node_id
-        """).format(_nodeType = nodeType, _name = name)
+    def merge_node_tx(tx, name, nodeType):
+        create_node = """
+            MERGE (n:%(_nodeType)s {name: "%(_name)s"})
+            RETURN n.name AS name
+        """ % {"_nodeType": nodeType, "_name": name}
         result = tx.run(create_node)
-        return result.single()["node_id"]
+        return result.single()["name"]
 
     def create(self, name, nodeType):
         with self._driver.session() as session:
-            result = session.execute_write(self.create_node_tx, name, nodeType)
+            result = session.execute_write(self.merge_node_tx, name, nodeType)
         return result
     
 if __name__ == "__main__":
