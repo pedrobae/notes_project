e2d85ef35d65ea3ca4b2ede498f1f8c5f3e4cd6d
Pedro Abe <pedrobettiolabe@gmail.com>
2024-12-05 07:03:12 -0300

    delete node (no relationship)

--- main/python/connection.py
+++ main/python/connection.py
@@ -10,19 +10,33 @@ class Connection():
         self._driver.close()
 
     @staticmethod
-    def merge_node_tx(tx, name, nodeType, property, value):
+    def merge_node_tx(tx, name, nodeType, attribute, value):
         merge_node = """
             MERGE (n:%(_nodeType)s {name: "%(_name)s"})
-            SET n.%(_property)s = "%(_value)s"
+            SET n.%(_attribute)s = "%(_value)s"
             RETURN n.name AS name
-        """ % {"_nodeType": nodeType, "_name": name, "_property": property, "_value": value}
+        """ % {"_nodeType": nodeType, "_name": name, "_attribute": attribute, "_value": value}
         result = tx.run(merge_node)
         return result.single()["name"]
+    
+    @staticmethod
+    def delete_node_tx(tx, name, nodeType):
+        delete_node = """
+            MATCH (n:%(_nodeType)s {name: "%(_name)s"})
+            DELETE n
+        """ % {"_nodeType": nodeType, "_name": name}
+        result = tx.run(delete_node)
+        return result
 
-    def merge(self, name, nodeType, propertiesDict):
-        for property, value in propertiesDict.items():
+    def merge(self, name, nodeType, attributesDict):
+        for attribute, value in attributesDict.items():
             with self._driver.session() as session:
-                result = session.execute_write(self.merge_node_tx, name, nodeType, property, value)
+                result = session.execute_write(self.merge_node_tx, name, nodeType, attribute, value)
+        return result
+    
+    def delete(self, name, nodeType):
+        with self._driver.session() as session:
+            result = session.execute_write(self.delete_node_tx, name, nodeType)
         return result
     
 if __name__ == "__main__":
@@ -33,4 +47,6 @@ if __name__ == "__main__":
     con = Connection(url, user_, password_)
 
 #   Testing connection and node merger
-    con.merge("Neza", "Character", {"shard": "life"})
+#    con.merge("Neza", "Character", {"birthYear": "10.000 A.C", "birthPlace": "Imperio Cobre Ardente"})
+
+    con.delete("Neza", "Character")
