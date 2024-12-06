c48ba5c7be8cc16c0f3fa27580c1c1d4d1e66b21
Pedro Abe <pedrobettiolabe@gmail.com>
2024-12-06 03:35:28 -0300

    separating the connection test from the class

--- main/python/connection.py
+++ main/python/connection.py
@@ -1,6 +1,4 @@
-import os
 from neo4j import GraphDatabase
-from dotenv import load_dotenv
 
 class Connection():
     def __init__(self, uri, user, password):
@@ -100,30 +98,3 @@ class Connection():
                     self.merge_edge_tx, name_1, nodeType_1, name_2, nodeType_2, property, value
                     )
         return result
-    
-if __name__ == "__main__":
-    load_dotenv()
-    url = "neo4j://localhost:7687"
-    user_ = os.environ.get("NEO4J_USER")
-    password_ = os.environ.get("NEO4J_PASSWORD")
-    con = Connection(url, user_, password_)
-
-#   Testing node merger
-#    con.merge("Neza", "Character", {"birthYear": "10.000 B.C", "birthPlace": "Ardent Copper Empire"})
-#    con.merge("Gelboss", "Character", {"shard": "Autonomy", "summary": "One of the ten genesis shards"})
-
-#   Testing deletion
-#    con.delete("Neza", "Character")
-
-#   Testing read
-    node, edges = con.read("Neza", "Character")
-    print(node)
-    print(edges)
-
-#   Testing search
-#    data = con.search_node("e")
-#    print(data)
-
-#   Testing edge merger
-#    con.merge_edge("Gelboss", "Character", "Neza", "Character", {"type": "Passion"})
-#    con.merge_edge("Neza", "Character", "Death", "Character", {"type":"Herald", "summary" : "Neza proposed a bargain to become the Herald of Death in exchange for seeing her lost daughter"})
