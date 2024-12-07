dc23abb0d7ed1ba666f7238d2de8a0d12343d169
Pedro Abe <pedrobettiolabe@gmail.com>
2024-12-07 12:44:20 -0300

    starting html

--- main/python/app.py
+++ main/python/app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, render_template, redirect, request, jsonify, flash
 
 from helpers import Node
 
@@ -15,3 +15,23 @@ url = "neo4j://localhost:7687"
 user_ = os.environ.get("NEO4J_USER")
 password_ = os.environ.get("NEO4J_PASSWORD")
 con = Connection(url, user_, password_)
+
+activeNode = Node(con)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/autocomplete", methods=["GET"])
+def autocomplete():
+    writing = request.args.get('q')
+    search = con.search_node(writing)
+    results = [node["name"] for node in search]
+    return jsonify(matching_search = results)
+
+@app.route("/activateNode", methods=["GET", "POST"])
+def activateNode():
+    name = request.form.get("searchNode")
+
+    activeNode.setNode(name)
+    
