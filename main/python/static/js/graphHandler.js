a459b6b9d4e1e48bbe8bfab354303d1d7797e44d
Pedro Abe <pedrobettiolabe@gmail.com>
2025-01-04 10:02:43 -0300

    moving nodeColors to graphHandler

--- main/python/static/js/graphHandler.js
+++ main/python/static/js/graphHandler.js
@@ -1,3 +1,26 @@
+const palette = [
+    "#fff7cd", // Light yellow
+    "#ffd95a", // Golden yellow 
+    "#ffc83f", // Rich gold 
+    "#ffb347", // Deep honey 
+    "#d4a017", // Mustard yellow 
+];
+
+
+const labelColorMap = {};
+let colorIndex = 0;
+
+// Function to get or assign a color for a label
+function getColorForLabel(label) {
+    if (!labelColorMap[label]) {
+        labelColorMap[label] = palette[colorIndex];
+        colorIndex = (colorIndex + 1) % palette.length; // Loop through the palette
+    }
+    console.log(labelColorMap)
+    return labelColorMap[label];
+}
+
+
 let cy;
 
 window.onload = function() {
